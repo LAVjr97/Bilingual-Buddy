f762271e46b14da69c47a366dfb51a49253f2069
LAVjr97 <130868019+LAVjr97@users.noreply.github.com>
2025-03-07 00:12:35 -0800

    created mcq functionality
    Each question is given in a random order, and the multiple choices are also given in a random order, we're able to keep track of the number of correct answers that a student gives on their first try on each question, that'll be their "score" they'll see at the end of the quiz.

--- bilingual_buddy/lib/screens/dashboard_screen.dart
+++ bilingual_buddy/lib/screens/dashboard_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'lecture_quiz_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'useful_widgets.dart';
 import 'lecture_dashboard.dart';
