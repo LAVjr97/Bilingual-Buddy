541449ab231effe793a6083b9e56d70514cbe810
LAVjr97 <130868019+LAVjr97@users.noreply.github.com>
2025-04-10 20:39:11 -0700

    moved over github classroom to this repo (this is the most up to date version)

--- bilingual_buddy/lib/main.dart
+++ bilingual_buddy/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/landing_page.dart'; //landing page
@@ -6,6 +7,7 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseFirestore.instance.settings = const Settings(persistenceEnabled: true);
   
   runApp(MyApp());
 }
